540082a3adf46bc7ab626f6aef4c669bf14d5876
Craig Oliver <craig.robert.oliver@gmail.com>
2019-01-30 11:34:27 -0500

    WIP pylint

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+""" Reads a repo tree from GitHub, then produces a json file ready to be
+    imported into analyze-catalog for the BIEL website. """
+
 import argparse
 import json
 import operator
@@ -10,12 +13,13 @@ import urllib
 import github
 
 def main():
+    """ Main function. """
     args = parse_arguments()
-    github_api = login_to_github(args.user, args.password)
+    github_api = get_github_api(args.user, args.password)
     repo = github_api.get_repo("wa-biel/biel-files")
-    tree = repo.get_git_tree("master",recursive=True)
+    tree = repo.get_git_tree("master", recursive=True)
     biel_data = get_biel_data(tree)
-    json.dump(biel_data,args.outfile, sort_keys=True, indent=4)
+    json.dump(biel_data, args.outfile, sort_keys=True, indent=4)
 
 def parse_arguments(): # pragma: no cover
     """ Configures and parses command-line arguments """
@@ -41,7 +45,9 @@ def parse_arguments(): # pragma: no cover
 
     return argparser.parse_args()
 
-def login_to_github(username, password):
+def get_github_api(username, password):
+    """ Logs into GitHub and returns an api object. If username and
+        password are both blank, then an anonymous login will be used. """
     if (username == "" and password == ""):
         github_api = github.Github()
     else:
@@ -49,11 +55,12 @@ def login_to_github(username, password):
     return github_api
 
 def get_biel_data(tree):
+    """ Reads the repo tree and returns a BIEL-formatted data object. """
     files = {}
     for entry in tree.tree:
         path_parts = pathlib.Path(entry.path).parts
         # Ignore anything outside the review guide
-        if (path_parts[0] != "review-guide"):
+        if path_parts[0] != "review-guide":
             continue
         # Only consider .docx and .pdf files
         filename = path_parts[-1]
@@ -81,20 +88,20 @@ def get_biel_data(tree):
             "path": entry.path,
             }
     data = []
-    en = {}
-    data.append(en)
-    en["code"] = "en"
-    en["contents"] = []
-    en_contents = en["contents"]
-    rg = {}
-    en_contents.append(rg)
-    rg["code"] = "rg"
-    rg["name"] = "Reviewer's Guide"
-    rg["subject"] = "Reference"
-    rg["checkingLevel"] = "3"
-    rg["links"] = []
-    rg["subcontents"] = []
-    subcontents = rg["subcontents"]
+    en_node = {}
+    data.append(en_node)
+    en_node["code"] = "en"
+    en_node["contents"] = []
+    en_node_contents = en_node["contents"]
+    rg_node = {}
+    en_node_contents.append(rg_node)
+    rg_node["code"] = "rg"
+    rg_node["name"] = "Reviewer's Guide"
+    rg_node["subject"] = "Reference"
+    rg_node["checkingLevel"] = "3"
+    rg_node["links"] = []
+    rg_node["subcontents"] = []
+    subcontents = rg_node["subcontents"]
     sort = 0
     for file_data in sorted(files.values(), key=operator.itemgetter("sort")):
         sort += 1
@@ -107,16 +114,17 @@ def get_biel_data(tree):
             }
         for link in file_data["links"].values():
             entry["links"].append({
-                    "url": path_to_url(link["path"]),
-                    "format": link["extension"],
-                    "zipContent": "",
-                    "quality": None,
-                    "chapters": []
-                    })
+                "url": path_to_url(link["path"]),
+                "format": link["extension"],
+                "zipContent": "",
+                "quality": None,
+                "chapters": []
+                })
         subcontents.append(entry)
     return data
 
 def path_to_url(path):
+    """ Returns a URL for the given path that will download the file from the repo. """
     return "https://github.com/wa-biel/biel-files/raw/master/" + urllib.parse.quote(path)
 
 if __name__ == "__main__":
